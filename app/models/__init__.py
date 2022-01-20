63fb5c11511cd365c24e8c289133f8e1024bb8af
Daniel Flores <daniel.flores4930@gmail.com>
2022-01-20 09:08:45 -0800

    fixed database for lists and posta and updated schema again

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,4 @@
 from .db import db
 from .user import User
 from .category import Category
-from .list import List
-from .post import Post
+from .list_post import List, Post
---
+++ app/models/list_post.py
@@ -0,0 +1,75 @@
+from .db import db
+
+posts_lists = db.Table(
+    "posts_lists",
+    db.Column(
+        "post_id", 
+        db.Integer, 
+        db.ForeignKey("posts.id"), 
+        primary_key=True
+    ),
+    db.Column(
+        "list_id", 
+        db.Integer, 
+        db.ForeignKey("lists.id"), 
+        primary_key=True
+    )
+)
+
+class List(db.Model):
+    __tablename__ = "lists"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    image_url = db.Column(db.String(255))
+
+    user = db.relationship('User', back_populates='lists', cascade="all, delete")
+    posts = db.relationship(
+        'Post',
+        secondary=posts_lists,
+        back_populates="lists"
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "user_id": self.user_id,
+            "image_url": self.image_url
+        }
+
+
+class Post(db.Model):
+    __tablename__='posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    title = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    
+    user = db.relationship('User', back_populates="posts")
+    category = db.relationship('Category', back_populates="posts")
+
+    lists = db.relationship(
+        'List',
+        secondary=posts_lists,
+        back_populates="posts"
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "category_id": self.category_id,
+            "image_url": self.image_url,
+            "description": self.description,
+            "price": self.price,
+            "quantity": self.quantity
+        }
