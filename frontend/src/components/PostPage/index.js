cafde049db907dea72f43fde0ec64c66be5d7658
Daniel Flores <daniel.flores4930@gmail.com>
2022-11-02 19:52:59 -0700

    fixes to homepage and single post page

--- frontend/src/components/PostPage/index.js
+++ frontend/src/components/PostPage/index.js
@@ -27,15 +27,15 @@ const PostPage = props => {
     const [offer, setOffer] = useState(post?.price);
     const category = categories.find(category => category.id === post?.category_id);
     const similarPosts = posts.filter(similarPost => {
-        if(similarPost.category_id === category?.id){
-            if(similarPost.id !== post.id) return similarPost;
+        if (similarPost.category_id === category?.id) {
+            if (similarPost.id !== post.id) return similarPost;
         }
         return null;
     });
     const seller = users?.find(user => user.id === post?.user_id);
     const otherSellerPosts = posts.filter(sellerPost => {
-        if(sellerPost.user_id === seller?.id){
-            if(sellerPost.id !== post.id) return sellerPost;
+        if (sellerPost.user_id === seller?.id) {
+            if (sellerPost.id !== post.id) return sellerPost;
         }
         return null;
     });
@@ -43,16 +43,16 @@ const PostPage = props => {
     const postListsSet = new Set();
     let existingChat = null;
 
-    for(let i = 0; i < postLists?.length; i++) {
+    for (let i = 0; i < postLists?.length; i++) {
         postListsSet.add(postLists[i].id);
     }
 
-    for(let i = 0; i < myLists.length; i++) {
-        if(!postListsSet.has(myLists[i].id)) listsToAdd.add(myLists[i]);
+    for (let i = 0; i < myLists.length; i++) {
+        if (!postListsSet.has(myLists[i].id)) listsToAdd.add(myLists[i]);
     }
 
-    for(let key in chats) {
-        if(chats[key].post_id === post.id) {
+    for (let key in chats) {
+        if (chats[key].post_id === post.id) {
             existingChat = chats[key];
         }
     }
@@ -66,18 +66,18 @@ const PostPage = props => {
         dispatch(getChats());
 
         async function fetchData() {
-          const response = await fetch('/api/users/');
-          const responseData = await response.json();
-          setUsers(responseData.users);
+            const response = await fetch('/api/users/');
+            const responseData = await response.json();
+            setUsers(responseData.users);
         }
         fetchData();
 
-        (function () {document.documentElement.scrollTop = 0})();
+        (function () { document.documentElement.scrollTop = 0 })();
 
-        if(window.location.href.split('/').length === 5) {
-          if (window.location.href.split('/')[3] === 'posts') {
-            document.getElementById('create-post-button')?.setAttribute('hidden', true);
-          }
+        if (window.location.href.split('/').length === 5) {
+            if (window.location.href.split('/')[3] === 'posts') {
+                document.getElementById('create-post-button')?.setAttribute('hidden', true);
+            }
         }
 
         document.getElementById('about-links').setAttribute('style', 'display: none');
@@ -86,7 +86,7 @@ const PostPage = props => {
     const makeOffer = event => {
         event.preventDefault();
         setShowOfferModal(false);
-        if(!existingChat) dispatch(createOffer(offer.toLocaleString("en-US"), post));
+        if (!existingChat) dispatch(createOffer(offer.toLocaleString("en-US"), post));
         else dispatch(sendMessage(existingChat.id, `Hi there! I'm interested in your ${post?.title}. Would you take $${offer.toLocaleString("en-US")}?`));
     }
 
@@ -109,52 +109,55 @@ const PostPage = props => {
     return (
         <div className="post-container">
             <div className="upper-container">
-                <div className="left-container top-container-mobile">
-                    <div className="line-div-1"></div>
+                <div className="left-container top-container-mobile flx-ctr">
                     <img src={post?.image_url} alt="post" className="post-page-image"></img>
                 </div>
                 <div className="right-container bottom-container-mobile">
-                    <h2 className="post-page-title">{post?.title}</h2>
-                    <h2 className="post-page-price">${post?.price.toLocaleString("en-US")}</h2>
-                    <div className="quantity-category-container">
-                        <label className="category-page-text">{category?.name}</label>
-                        {/* <label className="quantity-text">Quantity: </label>
-                        <h2 className="quantity-number">{post?.quantity}</h2> */}
-                    </div>
-                    {user && (
-                        <div className="post-buttons">
-                            {user.id !== post?.user_id && (
-                                <>
-                                    <button className="button-default" onClick={event => setShowOfferModal(true)}>Offer</button>
-                                    <button className="add-to-list-button" onClick={event => setShowListsToAdd(true)}>ADD TO LIST<span className="material-icons list-icon">lists</span></button>
-                                </>
-                            )}
-                            {user.id === post?.user_id && window.innerWidth < 600 && (
-                                <>
-                                    <button className="button-default" onClick={() => setEditButtonPopup(true)}>Edit</button>
-                                    <button className="button-default-cancel" onClick={handleDelete}>Delete</button>
-                                </>
-                            )}
+                    <div className="flx-ctr flx-col" style={{ justifyContent: 'flex-start' }}>
+                        <h2 className="post-page-title">{post?.title}</h2>
+                        <h2 className="post-page-price">${post?.price.toLocaleString("en-US")}</h2>
+                        <div className="quantity-category-container">
+                            <label className="category-page-text">{category?.name}</label>
+                            {/* <label className="quantity-text">Quantity: </label>
+                            <h2 className="quantity-number">{post?.quantity}</h2> */}
                         </div>
-                    )}
-                    {!user && (
-                        <div className="post-buttons" />
-                    )}
-                    <div className="seller-container">
-                        <div className="line-div-2"></div>
-                        <div className="line-div-3"></div>
-                        <NavLink to={`/users/${seller?.id}`}>
-                            <img src={seller?.profile_pic_url} alt="seller-profile" className="seller-profile-pic" onClick={event => document.getElementById("about-links").setAttribute("style", "display: flex")}></img>
-                        </NavLink>
-                        <h2 className='seller-username' onClick={event => document.getElementById("about-links").setAttribute("style", "display: flex")}>
-                            <NavLink to={`/users/${seller?.id}`}>{seller?.username}</NavLink>
-                        </h2>
+                        {user && (
+                            <div className="post-buttons">
+                                {user.id !== post?.user_id && (
+                                    <>
+                                        <button className="button-default" onClick={event => setShowOfferModal(true)}>Offer</button>
+                                        <button className="add-to-list-button" onClick={event => setShowListsToAdd(true)}>ADD TO LIST<span className="material-icons list-icon">lists</span></button>
+                                    </>
+                                )}
+                                {user.id === post?.user_id && window.innerWidth < 600 && (
+                                    <>
+                                        <button className="button-default" onClick={() => setEditButtonPopup(true)}>Edit</button>
+                                        <button className="button-default-cancel" onClick={handleDelete}>Delete</button>
+                                    </>
+                                )}
+                            </div>
+                        )}
+                        {!user && (
+                            <div className="post-buttons" />
+                        )}
+                        <div className="seller-container">
+                            <div className="line-div-2"></div>
+                            <div className="line-div-3"></div>
+                            <NavLink to={`/users/${seller?.id}`}>
+                                <img src={seller?.profile_pic_url} alt="seller-profile" className="seller-profile-pic" onClick={event => document.getElementById("about-links").setAttribute("style", "display: flex")}></img>
+                            </NavLink>
+                            <h2 className='seller-username' onClick={event => document.getElementById("about-links").setAttribute("style", "display: flex")}>
+                                <NavLink to={`/users/${seller?.id}`}>{seller?.username}</NavLink>
+                            </h2>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className="line-div-container">
+                <div className="line-div-4" />
+            </div>
             <div className="bottom-container">
                 <div className="description-container">
-                    <div className="line-div-4"></div>
                     <h2 className="description-title">Description</h2>
                     <p className="description-text">{post?.description}</p>
                 </div>
@@ -163,7 +166,7 @@ const PostPage = props => {
                     <div className="similar-posts-div-container">
                         {similarPosts?.map(post => (
                             <div className="similar-post-div" key={post.id}>
-                                <NavLink to={`/posts/${post.id}`} exact={true} activeClassName='active' onClick={function () {document.documentElement.scrollTop = 0}}>
+                                <NavLink to={`/posts/${post.id}`} exact={true} activeClassName='active' onClick={function () { document.documentElement.scrollTop = 0 }}>
                                     <img src={post.image_url} alt="similar-post" className={`image-post ${post.id}`}></img>
                                 </NavLink>
                                 <h3 className="similar-post-title">{post.title}</h3>
@@ -177,7 +180,7 @@ const PostPage = props => {
                     <div className="more-posts-div-container">
                         {otherSellerPosts?.map(post => (
                             <div className="more-post-div" key={post.id}>
-                                <NavLink to={`/posts/${post.id}`} exact={true} activeClassName='active' onClick={function () {document.documentElement.scrollTop = 0}}>
+                                <NavLink to={`/posts/${post.id}`} exact={true} activeClassName='active' onClick={function () { document.documentElement.scrollTop = 0 }}>
                                     <img src={post.image_url} alt="more-post" className={`image-post ${post.id}`}></img>
                                 </NavLink>
                                 <h3 className="more-post-title">{post.title}</h3>
@@ -189,7 +192,7 @@ const PostPage = props => {
             </div>
             {showListsToAdd && (
                 <div>
-                    <div className="background-modal" onClick={() => setShowListsToAdd(false)}/>
+                    <div className="background-modal" onClick={() => setShowListsToAdd(false)} />
                     <div className="add-list-post-container">
                         <h2>Your Lists!</h2>
                         <div className="list-buttons-container">
@@ -207,17 +210,17 @@ const PostPage = props => {
                     <div className="offer-post-container add-list-post-container">
                         <h2>Offer</h2>
                         <div>
-                            <label style={{fontFamily: "'Fredoka One', cursive"}}>$</label>
+                            <label style={{ fontFamily: "'Fredoka One', cursive" }}>$</label>
                             <input required type="number" className="offer-input" placeholder={post?.price} onChange={event => setOffer(event.target.value)} defaultValue={post?.price} value={offer}></input>
                         </div>
                         <button className="button-default" onClick={makeOffer}>Offer</button>
                         <button className="button-default-cancel" onClick={e => setShowOfferModal(false)}>Cancel</button>
                     </div>
                 </div>
-        )}
-        {editButtonPopup && (
-            <EditPost post={post} trigger={editButtonPopup} setTrigger={setEditButtonPopup} />
-        )}
+            )}
+            {editButtonPopup && (
+                <EditPost post={post} trigger={editButtonPopup} setTrigger={setEditButtonPopup} />
+            )}
         </div>
     )
 }
